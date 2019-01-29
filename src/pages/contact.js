f011ae7f72f94c3d87b18d924e6806a4a712b075
FarahMint <zfarah.el@gmail.com>
2019-01-29 21:50:18 +0000

    navbar  added

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,17 +1,14 @@
 import React from 'react'
 // import { Link } from 'gatsby'
-import { FaBeer } from 'react-icons/fa'
 import Layout from '../components/layout'
 // import Image from '../components/image'
 import SEO from '../components/seo'
 
-const IndexPage = () => (
+const ContactPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h3>
-      Hi Gatsby <FaBeer />
-    </h3>
+    <h3>Contact Page</h3>
   </Layout>
 )
 
-export default IndexPage
+export default ContactPage
--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,17 +1,15 @@
 import React from 'react'
 // import { Link } from 'gatsby'
-import { FaBeer } from 'react-icons/fa'
+
 import Layout from '../components/layout'
 // import Image from '../components/image'
 import SEO from '../components/seo'
 
-const IndexPage = () => (
+const MenuPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h3>
-      Hi Gatsby <FaBeer />
-    </h3>
+    <h3>Menu Page</h3>
   </Layout>
 )
 
-export default IndexPage
+export default MenuPage
